bf1ee9c80200df3cce7f29e7a91b6f17e3a65a9b
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-09-05 21:36:11 +0200

    Update: Index.ts file basic connection

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: "Welcome to the Shopping List API!" }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
